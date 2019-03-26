3c47497fd87ac74ee4285504a495a2ac0516e2c2
Ryan <thechanceofrain@gmail.com>
2019-03-26 14:07:54 -0500

    honestly idk what all these changes are

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+    <main-navigation />
+</template>
+
+<script>
+import { StackNavigator } from "vue-native-router";
+import MenuScreen from "./screens/MenuScreen.vue";
+
+const MainNavigation = StackNavigator(
+    {
+        Menu: MenuScreen
+    },
+    {
+        initialRouteName: "Menu"
+    }
+)
+
+export default {
+    components: {
+        MainNavigation
+    }
+}
+</script>
---
+++ screens/MenuScreen.vue
@@ -0,0 +1,29 @@
+<template>
+    <view class="container">
+        <text>This is the menu screen.</text>
+    </view>
+</template>
+    
+<script>
+export default {
+    props: {
+        navigation: {
+            type: Object
+        }
+    },
+    data: function() {
+        return {
+
+        };
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+.container {
+    background-color: white;
+}
+</style>
