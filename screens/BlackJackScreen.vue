522e1bce2efeb24a436778b75877776adbb6979c
Ryan <thechanceofrain@gmail.com>
2019-03-26 15:10:56 -0500

    added BlackJackScreen

---
+++ screens/BlackJackScreen.vue
@@ -0,0 +1,41 @@
+<template>
+    <view class="container">
+        <text>This is the blackjack gameplay screen.</text>
+    </view>
+</template>
+    
+<script>
+const BlackJack = require('../logic/BlackJack');
+const Deck = require('../logic/Deck');
+const Dealer = require('../logic/Dealer');
+const Player = require('../logic/Player');
+
+export default {
+    props: {
+        navigation: {
+            type: Object
+        }
+    },
+    data: function() {
+        return {
+            deck: null,
+            dealer: null,
+            player: null
+        };
+    },
+    created: function() {
+        this.deck = Deck();
+        this.dealer = Dealer();
+        this.player = Player();
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+.container {
+    background-color: white;
+}
+</style>
