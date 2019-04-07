60542b1434159892542e4ef8ada17088e6071e0a
Ryan <thechanceofrain@gmail.com>
2019-04-07 15:24:23 -0500

    Fixed some stuff in Player

--- logic/Player.js
+++ logic/Player.js
@@ -3,46 +3,40 @@ module.exports = function(playerMoney){
         Hand: [],
         PMoney: playerMoney,
 
-        bet: function(player,InitBet){
+        bet: function(player,InitBet) {
             //TODO
         },
-        startHand: function(deckStack){
+        startHand: function(deckStack) {
             this.Hand = [];
             this.hit(deckStack);
             this.hit(deckStack);
         },
-        is21: function(player) {//player win condition
-            var sum =  calPCardVals(player);
-            if (sum[0]==21||sum[1]==21){
-                return true;
-            }
-            return false;
+        is21: function() {//player win condition
+            let sum =  this.calCardVals();
+            return sum[0] == 21 || sum[1] == 21;
         },
-        isBust: function(player){
-            var sum = calPCardVals(player);
-            if(sum[0]>21){
-                return true;
-            }
-            return false;
+        isBust: function() {
+            let sum = this.calCardVals();
+            return sum[0] > 21;
         },
-        hit: function(deckStack){
+        hit: function(deckStack) {
             this.Hand.push(deckStack.pop());
         },
-        doubleDown: function(player,runBet){
+        doubleDown: function(runBet) {
             //TODO
         },
 
-        calCardVals: function(player){//player card vals
-            var sum=[0,0];
-            for(let i = 0; i < player.PStack.length; i++){//for the length of the hand
-                var card = player.PStack[i];
-                var cardN = card.slice(0,card.length-1);
-                if(cardN.equals('1')){//if card is Ace
+        calCardVals: function() {//player card vals
+            let sum=[0,0];
+            for(let i = 0; i < player.PStack.length; i++) {//for the length of the hand
+                let card = player.PStack[i];
+                let cardN = card.slice(0,card.length-1);
+                if(cardN.equals('1')) {//if card is Ace
                     sum[0]+= 1;
                     sum[1]+= sum[0]+11;//optional val of Ace
                     
                 }
-                else if(cardN.length==1){//if card is 2-9 inclusive
+                else if(cardN.length==1) {//if card is 2-9 inclusive
                     sum[0]+= parseInt(cardN);
                     if(sum[1]!=0){//if Ace is present
                         sum[1]+= parseInt(cardN);
